a676eafcff9c5cf038fb553fcc6679d57204a299
Asunaaaaaaaaaaa <fjj1171464952@gmail.com>
2026-04-13 12:29:17 +0800

    fixed DWA

--- path_planning/dynamic_window_approach.py
+++ path_planning/dynamic_window_approach.py
@@ -42,7 +42,7 @@ class Config:
 
         # if robot_type == RobotType.rectangle
         self.robot_width = 0.5  # [m] for collision check
-        self.robot_length = 1.2  # [m] for collision check
+        self.robot_length = 1.2
         # obstacles [x(m), y(m)]
         self.ob = np.array([[-1, -1],
                             [0, 2],
@@ -113,18 +113,126 @@ def predict_trajectory(x_init, v, y, config):
 
 def calc_control_and_trajectory(x, dw, config, goal, ob):
     x_init = x[:]
+    min_cost = float('inf')
+    best_u = [0.0, 0.0]
+    best_trajectory = np.array([x])
     
+    # evaluate all the trajectory with sampled input
+    for v in np.arange(dw[0], dw[1], config.v_resolution):
+        for y in np.arange(dw[2], dw[3], config.yaw_rate_resolution):
 
+            trajectory = predict_trajectory(x_init, v, y, config)
+            # calc cost
+            to_goal_cost = config.to_goal_cost_gain * calc_to_goal_cost(trajectory, goal)
+            speed_cost = config.speed_cost_gain * (config.max_speed - trajectory[-1, 3])
+            ob_cost = config.obstacle_cost_gain * calc_obstacle_cost(trajectory, ob, config)
 
+            final_cost = to_goal_cost + speed_cost + ob_cost
 
+            # search minimum trajectory
+            if min_cost >= final_cost:
+                min_cost = final_cost
+                best_u = [v, y]
+                best_trajectory = trajectory
+                
+                # need to turn around
+                if abs(best_u[0]) < config.robot_stuck_flag_cons \
+                        and abs(x[3]) < config.robot_stuck_flag_cons:
+                    best_u[1] = -config.max_delta_yaw_rate
+    return best_u, best_trajectory
 
+# collision detection
+def calc_obstacle_cost(trajectory, ob, config):
+    ox = ob[:, 0]
+    oy = ob[:, 1]
+    dx = trajectory[:, 0] - ox[:, None]
+    dy = trajectory[:, 1] - oy[:, None]
+    r = np.hypot(dx, dy)
 
+    if config.robot_type == RobotType.rectangle:
+        yaw = trajectory[:, 2]
+        rot = np.array([[np.cos(yaw), -np.sin(yaw)], [np.sin(yaw), np.cos(yaw)]])
+        rot = np.transpose(rot, [2, 0, 1])
+        local_ob = ob[:, None] - trajectory[:, 0:2]
+        local_ob = local_ob.reshape(-1, local_ob.shape[-1])
+        local_ob = np.array([local_ob @ x for x in rot])
+        local_ob = local_ob.reshape(-1, local_ob.shape[-1])
+        upper_check = local_ob[:, 0] <= config.robot_length / 2
+        right_check = local_ob[:, 1] <= config.robot_width / 2
+        bottom_check = local_ob[:, 0] >= -config.robot_length / 2
+        left_check = local_ob[:, 1] >= -config.robot_width / 2
+        if (np.logical_and(np.logical_and(upper_check, right_check),
+                           np.logical_and(bottom_check, left_check))).any():
+            return float("Inf")
+    elif config.robot_type == RobotType.circle:
+        if np.array(r <= config.robot_radius).any():
+            return float("Inf")
 
-def main():
+    min_r = np.min(r)
+    return 1.0 / (min_r + 0.01)  # Add small value to avoid division by zero
+
+# angle difference
+def calc_to_goal_cost(trajectory, goal):
+    dx = goal[0] - trajectory[-1, 0]
+    dy = goal[1] - trajectory[-1, 1]
+    error_angle = math.atan2(dy, dx)
+    cost_angle = error_angle - trajectory[-1, 2]
+    cost = abs(math.atan2(math.sin(cost_angle), math.cos(cost_angle)))
+
+    return cost
+
+def plot_arrow(x, y, yaw, length=0.5, width=0.1):
+    plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
+              head_length=width, head_width=width)
+    plt.plot(x, y)
     
+def plot_robot(x, y, yaw, config):
+    circle = plt.Circle((x, y), config.robot_radius, color="yellow")
+    plt.gcf().gca().add_artist(circle)
+    out_x, out_y = (np.array([x, y]) +
+                    np.array([np.cos(yaw), np.sin(yaw)]) * config.robot_radius)
+    plt.plot([x, out_x], [y, out_y], "-k")
+    
+    
+def main(gx=10.0, gy=10.0, robot_type=RobotType.circle, show_animation=True):
+    print("start!")
+    x = np.array([0.0, 0.0, math.pi / 8.0, 0.0, 0.0])
+    goal = np.array([gx, gy])
+    config.robot_type = robot_type
+    trajectory = np.array(x)
+    ob = config.ob
+    
+    while True:
+        u, predicted_trajectory = dwa_control(x, config, goal, ob)
+        x = motion(x, u, config.dt)
+        trajectory = np.vstack((trajectory, x))
+        
+        if show_animation:
+            plt.cla()
+            plt.gcf().canvas.mpl_connect(
+                'key_release_event',
+                lambda event: [exit(0) if event.key == 'escape' else None])
+            
+            plt.plot(predicted_trajectory[:, 0], predicted_trajectory[:, 1], "-g")
+            plt.plot(x[0], x[1], "xr")
+            plt.plot(goal[0], goal[1], "xb")
+            plt.plot(ob[:, 0], ob[:, 1], "ok")
+            plot_robot(x[0], x[1], x[2], config)
+            plot_arrow(x[0], x[1], x[2])
+            plt.axis("equal")
+            plt.grid(True)
+            plt.pause(0.0001)
 
-
-
+        # check reaching goal
+        dist_to_goal = math.hypot(x[0] - goal[0], x[1] - goal[1])
+        if dist_to_goal <= config.robot_radius:
+            print("Goal!!")
+            break
+    
+    if show_animation:
+        plt.plot(trajectory[:, 0], trajectory[:, 1], "-r")
+        plt.pause(0.0001)
+        plt.show()
 
 
 if __name__ == '__main__':
